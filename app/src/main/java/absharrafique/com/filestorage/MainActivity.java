8bbc7fbdc654719c26628549353931dc3f610812
DREAMORBIT\abshar.rafique <abshahar.rafique@dreamorbit.com>
2017-12-08 13:31:59 +0530

    Added a comment to the Main Activity file and made a new class called Test.

--- app/src/main/java/absharrafique/com/filestorage/MainActivity.java
+++ app/src/main/java/absharrafique/com/filestorage/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Look this file i'm linking.
         setContentView(R.layout.activity_main);
         editText = (EditText) findViewById(R.id.editText1);
     }
---
+++ app/src/main/java/absharrafique/com/filestorage/Test.java
@@ -0,0 +1,9 @@
+package absharrafique.com.filestorage;
+
+/**
+ * Created by abshar.rafique on 12/8/2017.
+ */
+
+public class Test {
+    public String sms;
+}
